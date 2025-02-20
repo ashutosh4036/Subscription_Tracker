3ed82ae587e04429abdd2ab5196fe331141ca2b7
ashutosh4036 <ashutosh4036@gmail.com>
2025-02-20 11:57:48 +0530

    Created express server and added env file for smooth development and ddeployment at production level as well

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {PORT} from './config/env.js'
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Subscription tracker api");
+});
+
+app.listen(PORT, () => {
+    console.log(`Subscription tracker api is running on http://localhost:${PORT}`);
+
+});
+
+export default app;
